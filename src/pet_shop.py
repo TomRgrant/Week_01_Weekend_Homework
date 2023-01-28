9b61a357c0ead944dd048679d8c7be01a719877e
Tom Grant <grntthomasronnie@gmail.com>
2023-01-28 14:27:05 +0000

    Add first 6 functions

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -3,15 +3,26 @@
 def get_pet_shop_name(pet_shop):
     return pet_shop['name']
 
+def get_total_cash(pet_shop):
+    shop_total = pet_shop['admin']['total_cash']
+    return shop_total
 
+def add_or_remove_cash(pet_shop, cash_add):
+    old_total = pet_shop['admin']['total_cash']
+    new_total = old_total + cash_add
+    pet_shop['admin']['total_cash'] = new_total
+    
+def get_pets_sold(pet_shop):
+    return pet_shop['admin']['pets_sold']
 
+def increase_pets_sold(pet_shop, num):
+    pet_shop['admin']['pets_sold'] = pet_shop['admin']['pets_sold'] + num
 
-
-
-
-
-
-
+def get_stock_count(pet_shop):
+    counter = 0
+    for pet in pet_shop['pets']:
+        counter += 1
+    return counter
 
 
 
